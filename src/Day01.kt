a4c7547b72dd046efa642d3b9c4afc712802519c
Aleksander Brzozowski <olek.brzozowski@gmail.com>
2021-12-04 12:19:37 +0100

    Day01 - finish second part

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
+    part1()
+    part2()
+}
+
+private fun part1() {
     val testInputResult = readIntInput("Day01_test").measurementDepthIncreases()
     check(testInputResult == 3) { "Expect 3, found: $testInputResult" }
 
     val inputResult = readIntInput("Day01").measurementDepthIncreases()
-    println("Result: $inputResult")
+    println("Result 1: $inputResult")
 }
 
-private fun List<Int>.measurementDepthIncreases() =
-    withIndex()
-        .count { (index, value) ->
-            when {
-                index == 0 -> false
-                this[index - 1] < value -> true
-                else -> false
-            }
-        }
+private fun part2() {
+    val testInputResult = readIntInput("Day01_test").measureWindowedDepthIncreases()
+    check(testInputResult == 2) { "Expected: 2, found: $testInputResult" }
+    val inputResult = readIntInput("Day01").measureWindowedDepthIncreases()
+    println("Result 2: $inputResult")
+}
+
+private fun List<Int>.measurementDepthIncreases(): Int =
+    windowed(2)
+        .count { (prev, current) -> prev < current }
+
+private fun List<Int>.measureWindowedDepthIncreases(): Int =
+    windowed(3) { measurements -> measurements.sum() }
+        .windowed(2)
+        .count { (prev, current) -> prev < current }
