2194fda608939ead8f43478d219535d374884b17
Aleksander Brzozowski <olek.brzozowski@gmail.com>
2021-12-19 00:43:51 +0100

    Day12 - part2

--- src/Day12.kt
+++ src/Day12.kt
@@ -5,21 +5,44 @@ import Graph.VertexType.START
 
 fun main() {
     part1()
+    part2()
 }
 
 private fun part1() {
+    val canVisitSmallCave: (String, Map<String, Int>) -> Boolean =
+        { point, visitedSmallCaves -> !visitedSmallCaves.containsKey(point) }
     val testInput = readGraph("Day12_test")
-    val testResult = testInput.numberOfPathsThatVisitSmallCavesAtMostOnce()
+    val testResult = testInput.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
     check(testResult == 10)
     val testInput1 = readGraph("Day12_test1")
-    val testResult1 = testInput1.numberOfPathsThatVisitSmallCavesAtMostOnce()
+    val testResult1 = testInput1.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
     check(testResult1 == 19)
     val testInput2 = readGraph("Day12_test2")
-    val testResult2 = testInput2.numberOfPathsThatVisitSmallCavesAtMostOnce()
+    val testResult2 = testInput2.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
     check(testResult2 == 226)
     val input = readGraph("Day12")
-    val result = input.numberOfPathsThatVisitSmallCavesAtMostOnce()
+    val result = input.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
     println("Number of paths that visit small caves at most once: $result")
+}
+
+private fun part2() {
+    val canVisitSmallCave: (String, Map<String, Int>) -> Boolean =
+        { point, visitedSmallCaves ->
+            val anySmallCaveVisitedTwice = visitedSmallCaves.values.any { it == 2 }
+            if (anySmallCaveVisitedTwice) !visitedSmallCaves.containsKey(point) else true
+        }
+    val testInput = readGraph("Day12_test")
+    val testResult = testInput.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
+    check(testResult == 36)
+    val testInput1 = readGraph("Day12_test1")
+    val testResult1 = testInput1.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
+    check(testResult1 == 103)
+    val testInput2 = readGraph("Day12_test2")
+    val testResult2 = testInput2.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
+    check(testResult2 == 3509)
+    val input = readGraph("Day12")
+    val result = input.numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave)
+    println("There are $result with this cave system")
 }
 
 private fun readGraph(file: String): Graph = readStringInput(file)
@@ -36,11 +59,11 @@ class Graph(input: List<Pair<String, String>>) {
             adjacentByPoints + mapOf(v1 to v1Adjacent, v2 to v2Adjacent)
         }
 
-    fun numberOfPathsThatVisitSmallCavesAtMostOnce(): Int {
+    fun numberOfPathsThatVisitSmallCavesAtMostOnce(canVisitSmallCave: (String, Map<String, Int>) -> Boolean): Int {
         val (start, startAdjacent) = adjacentByPoints.start()
         val visitedSmallCavesByMaybePaths =
             startAdjacent.mapTo(ArrayDeque()) { point ->
-                val visitedSmallCaves = if (point.toVertexType() == SMALL_CAVE) setOf(point) else emptySet()
+                val visitedSmallCaves = if (point.toVertexType() == SMALL_CAVE) mapOf(point to 1) else emptyMap()
                 listOf(start, point) to visitedSmallCaves
             }
         val paths = mutableListOf<List<String>>()
@@ -59,7 +82,7 @@ class Graph(input: List<Pair<String, String>>) {
                         if (adjacentPointType == START) {
                             continue
                         }
-                        if (adjacentPointType == SMALL_CAVE && adjacentPoint in visitedSmallCaves) {
+                        if (adjacentPointType == SMALL_CAVE && !canVisitSmallCave(adjacentPoint, visitedSmallCaves)) {
                             continue
                         }
                         if (adjacentPointType == END) {
@@ -67,7 +90,7 @@ class Graph(input: List<Pair<String, String>>) {
                             continue
                         }
                         val newVisitedSmallCaves =
-                            if (adjacentPointType == SMALL_CAVE) visitedSmallCaves + adjacentPoint else visitedSmallCaves
+                            if (adjacentPointType == SMALL_CAVE) visitedSmallCaves.withVisited(adjacentPoint) else visitedSmallCaves
                         val newMaybePath = maybePath + adjacentPoint
                         visitedSmallCavesByMaybePaths.addLast(newMaybePath to newVisitedSmallCaves)
                     }
@@ -77,6 +100,11 @@ class Graph(input: List<Pair<String, String>>) {
         }
 
         return paths.size
+    }
+
+    private fun Map<String, Int>.withVisited(point: String) = when (val count = this[point]) {
+        null -> this + (point to 1)
+        else -> this + (point to count + 1)
     }
 
     private fun AdjacentByPoints.start(): Pair<String, List<String>> =
